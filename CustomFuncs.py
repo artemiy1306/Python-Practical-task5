12a6f8e91b6dd19b6345645ef11c5ae8ce455e81
artemiy <karelskiy@mail.ru>
2023-06-07 23:30:51 +0300

    changes main and customfuncs

--- CustomFuncs.py
+++ CustomFuncs.py
@@ -1,3 +1,8 @@
-def power(a, b):
+def exponentiation(a, b):
   if b == 0: return 1
-  return power(a, b - 1) * a
+  return exponentiation(a, b - 1) * a
+
+def sum(a,b):
+    if b == 0:
+        return a
+    return sum(a,b-1) + 1
